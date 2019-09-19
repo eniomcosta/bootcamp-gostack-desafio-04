1d4b55cc1afbe010a8f20bcdcfa7e2c9f1bc1229
Enio Costa <enio.m.costa@gmail.com>
2019-09-18 22:06:28 -0300

    Trying to put a nice header :p

---
+++ src/components/main/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="logo">facebook</div>
+    </div>
+  );
+}
+
+export default Header;
